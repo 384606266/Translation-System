797c797da693163279a1df7bc8fe2a07a2d613f2
sgjzfzzf <sgjzfzzf@sjtu.edu.cn>
2022-06-29 10:48:31 +0800

    完成user部分的开发，对用户的增删查改和登陆进行过检查

---
+++ TranslationSystemBackend/src/main/java/com/example/translationsystembackend/TranslationSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.translationsystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslationSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TranslationSystemBackendApplication.class, args);
+    }
+
+}
---
+++ TranslationSystemBackend/src/test/java/com/example/translationsystembackend/TranslationSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.translationsystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslationSystemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
